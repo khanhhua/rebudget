c56a60b4f0fc031658429f18567f5a69cf653059
Khanh Hua <khanh.hua@pyco-group.com>
2016-09-17 14:32:32 +0800

    Clean up extra comments

--- src/containers/settings-component.jsx
+++ src/containers/settings-component.jsx
@@ -4,48 +4,6 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import {updateSettings} from '../actions';
 
-/*
-const SettingsComponent = (props) => {
-  // actions
-  const {updateSettings} = props;
-
-  const state = Object.assign({}, props.settings);
-
-  // Event handler
-  const onInputChange = (e) => {
-    console.log(e);
-    const name = e.target.name;
-    const value = e.target.value;
-
-    state[name] = value;
-  };
-
-  const onSaveClick = () => {
-    updateSettings(state);
-  };
-
-  return (
-    <div className="container">
-      <div className="col-xs-12">
-        <div className="form">
-          <div className="form-group">
-            <label htmlFor="input-currency" className="control-label">Currency</label>
-            <Field component={input}
-                   className="form-control"
-
-                   name="currency" id="input-currency" maxLength="3" placeholder="USD" />
-          </div>
-
-          <div className="form-group tex-center">
-            <button className="btn btn-success" onClick={onSaveClick}>Save</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-};
-*/
-
 class SettingsComponent extends React.Component {
   constructor (props) {
     super();
