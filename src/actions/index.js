7f897cb8d564bc82bec71779808173aa88ee0f4f
Khanh Hua <khanh.hua@pyco-group.com>
2016-09-24 02:25:09 +0800

    Apply jwt Authorization header

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,19 +3,26 @@
  */
 
 import request from 'superagent';
-import {INIT_DATA, LOGIN_FACEBOOK, UPDATE_SETTINGS, ADD_CATEGORY, ADD_SPENDING, ADD_INCOME, SELECT_CATEGORY} from './types';
+import {INIT_DATA, LOGIN_FACEBOOK, LINK, UPDATE_SETTINGS, ADD_CATEGORY, ADD_SPENDING, ADD_INCOME, SELECT_CATEGORY} from './types';
 
 const API_PREFIX = process.env.API_PREFIX || '';
 /*---------------------------------------------------------
 / ACTIONS
 /--------------------------------------------------------*/
 export const initData = () => (dispatch, getState) => {
+  const state = getState();
+  const {auth} = state.networkActivity;
+  if (!auth) {
+    return Promise.reject();
+  }
+  const {jwt} = auth;
+
   dispatch({ type: INIT_DATA, status: 'pending' });
 
   return Promise.all([
     new Promise((resolve, reject) =>
       request.get(`${API_PREFIX}/api/settings`)
-        .set('x-access-key', '0')
+        .set('Authorization', `jwt ${jwt}`)
         .send()
         .end((error, response) => {
           if (error) {
@@ -27,7 +34,7 @@ export const initData = () => (dispatch, getState) => {
         })),
     new Promise((resolve, reject) =>
       request.get(`${API_PREFIX}/api/categories`)
-        .set('x-access-key', '0')
+        .set('Authorization', `jwt ${jwt}`)
         .send()
         .end((error, response) => {
           if (error) {
@@ -39,7 +46,7 @@ export const initData = () => (dispatch, getState) => {
         })),
     new Promise((resolve, reject) =>
       request.get(`${API_PREFIX}/api/entries`)
-        .set('x-access-key', '0')
+        .set('Authorization', `jwt ${jwt}`)
         .send()
         .end((error, response) => {
           if (error) {
@@ -69,29 +76,24 @@ export const loginFacebook = () => (dispatch, getState) => {
       console.info(`[action:loginFacebook] User is already signed in.`);
     },
     () => {
-    var url = ['https://www.facebook.com/dialog/oauth?',
-      'client_id=1771952326416166',
-      process.env.NODE_ENV==='production'?
-        '&redirect_uri=http://rebudget-api.khanhhua.com/auth/facebook'
-        :
-        '&redirect_uri=http://localhost:8080/auth/facebook',
-      '&scope=email'].join('');
+      dispatch({ type: LOGIN_FACEBOOK, status: 'pending' });
 
-    dispatch({ type: LOGIN_FACEBOOK, status: 'pending' });
-    const childWindow = window.open(url, '_blank');
-
-    const interval = setInterval(() => {
-      if (!childWindow.closed) return;
-      clearInterval(interval);
-
-      login(false);
-    }, 500);
-  });
+      FB.login(response => {
+        if (response.authResponse) {
+          login(false);
+        }
+        else {
+          dispatch({ type: LOGIN_FACEBOOK, status: 'error', params: {error: 'Could not connect user to Facebook'}});
+        }
+      });
+    }
+  );
 
   function login (probe=true) {
     return new Promise((resolve, reject) => {
       FB.getLoginStatus((response) => {
         if (response.status === 'connected') {
+          const {accessToken} = response.authResponse;
           FB.api('/me', {fields: 'name'}, function(response) {
             if (response && response.error) {
               dispatch({ type: LOGIN_FACEBOOK, status: 'error', params: {error: 'Could not connect user to Facebook'}});
@@ -101,8 +103,9 @@ export const loginFacebook = () => (dispatch, getState) => {
             const {id: fbId, name} = response;
 
             dispatch({ type: LOGIN_FACEBOOK, status: 'success', params: {fbId, name}});
+
             // Load initial data only when logged in
-            dispatch(initData());
+            dispatch(link(accessToken));
             resolve();
           });
         }
@@ -120,12 +123,39 @@ export const loginFacebook = () => (dispatch, getState) => {
   }
 };
 
+export const link = (accessToken) => (dispatch, getState) => {
+  dispatch({type: LINK, status: 'pending'});
+
+  return new Promise(resolve =>
+    request.post(`${API_PREFIX}/api/link`)
+      .send({access_token: accessToken})
+      .end((error, response) => {
+        if (error) {
+          return dispatch({type: LINK, status: 'error', params: {error}});
+        }
+        let data = response.body;
+
+        dispatch({type: LINK, status: 'success', params: {jwt: data.jwt}});
+        dispatch(initData());
+        resolve();
+      })
+  );
+};
+
 export const updateSettings = (settings) => (dispatch, getState) => {
+  const state = getState();
+  const {auth} = state.networkActivity;
+  if (!auth) {
+    return Promise.reject();
+  }
+
+  const {jwt} = auth;
+
   dispatch({ type: UPDATE_SETTINGS, status: 'pending' });
 
   return new Promise(resolve =>
     request.post(`${API_PREFIX}/api/settings`)
-      .set('x-access-key', '0')
+      .set('Authorization', `jwt ${jwt}`)
       .send({settings})
       .end((error, response) => {
       if (error) {
@@ -140,13 +170,20 @@ export const updateSettings = (settings) => (dispatch, getState) => {
 };
 
 export const addCategory = (label) => (dispatch, getState) => {
+  const state = getState();
+  const {auth} = state.networkActivity;
+  if (!auth) {
+    return Promise.reject();
+  }
+
+  const {jwt} = auth;
   dispatch({ type: ADD_CATEGORY, status: 'pending' });
 
   let category = {label};
 
   return new Promise (resolve => {
     request.post(`${API_PREFIX}/api/categories`)
-      .set('x-access-key', '0')
+      .set('Authorization', `jwt ${jwt}`)
       .send(category).end((error, response) => {
       if (error) {
         return dispatch({ type: ADD_CATEGORY, status: 'error', params: {error} });
@@ -160,13 +197,21 @@ export const addCategory = (label) => (dispatch, getState) => {
 };
 
 export const addSpending = (spending) => (dispatch, getState) => {
+  const state = getState();
+  const {auth} = state.networkActivity;
+  if (!auth) {
+    return Promise.reject();
+  }
+
+  const {jwt} = auth;
+
   dispatch({ type: ADD_SPENDING, status: 'pending' });
 
   let entry = Object.assign({type: 'expense'}, spending);
 
   return new Promise(resolve =>
     request.post(`${API_PREFIX}/api/entries`)
-      .set('x-access-key', '0')
+      .set('Authorization', `jwt ${jwt}`)
       .send(entry).end((error, response) => {
       if (error) {
         return dispatch({ type: ADD_SPENDING, status: 'error', params: {error} });
@@ -180,13 +225,21 @@ export const addSpending = (spending) => (dispatch, getState) => {
 };
 
 export const addIncome = (income) => (dispatch, getState) => {
+  const state = getState();
+  const {auth} = state.networkActivity;
+  if (!auth) {
+    return Promise.reject();
+  }
+
+  const {jwt} = auth;
+
   dispatch({ type: ADD_INCOME, status: 'pending' });
 
   let entry = Object.assign({type: 'income'}, income);
 
   return new Promise(resolve =>
     request.post(`${API_PREFIX}/api/entries`)
-      .set('x-access-key', '0')
+      .set('Authorization', `jwt ${jwt}`)
       .send(entry).end((error, response) => {
       if (error) {
         return dispatch({ type: ADD_INCOME, status: 'error', params: {error} });
